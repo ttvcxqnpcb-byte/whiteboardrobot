b44efc4592f593ffb3c78a289d545528c1428cdb
曾暐勛 <weixunceng@cengweixundeMacBook-Air-7.local>
2026-06-16 15:11:42 +0800

    fix: hide 3D wireframe debug box when ArUco marker is not detected in the frame

--- config/robot_settings.py
+++ config/robot_settings.py
@@ -8,8 +8,8 @@ MARKER_SIZE = 1.0
 
 # 車體 3D 長方體相對於標籤中心的邊界比例 (可依實體狀況微調，寧可稍大)
 ROBOT_3D_FRONT  = 1.7   # 車頭往前凸出多少
-ROBOT_3D_BACK   = 1.5   # 車尾往後凸出多少
+ROBOT_3D_BACK   = 1.3   # 車尾往後凸出多少
 ROBOT_3D_LEFT   = 1.2   # 車身往左凸出多少
 ROBOT_3D_RIGHT  = 1.2   # 車身往右凸出多少
-ROBOT_3D_TOP    = 0.5   # 車頂高度 (最重要！決定傾斜時遮罩往外擴展的範圍)
-ROBOT_3D_BOTTOM = 0.0   # 車底貼地
+ROBOT_3D_TOP    = 1.1   # 車頂高度 (最重要！決定傾斜時遮罩往外擴展的範圍)
+ROBOT_3D_BOTTOM = 0.1   
